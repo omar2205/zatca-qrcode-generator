79a37ae67c4695d1919c49120fb5833c1d9b4b83
omar2205 <omar2205a@yahoo.com>
2022-09-27 22:15:15 +0000

    - add share feature - add material icons

--- src/components/Forms/Fatoora.svelte
+++ src/components/Forms/Fatoora.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { Block, Button, Link, List, ListInput, Toolbar, Sheet, Toast } from 'konsta/svelte'
+  import {
+    Block,
+    Button,
+    Link,
+    List,
+    Icon,
+    ListInput,
+    Toolbar,
+    Sheet,
+    Toast,
+  } from 'konsta/svelte'
   import Fatora, { iInvoice } from '../../models/Fatora'
+  import { shareFile } from '../../utils'
 
   let qr_url = ''
   let invoice_name = ''
@@ -20,20 +31,27 @@
     }
   }
 
-
   const get_invoice_name = () => {
     const now = new Date()
-    return `invoice_${now.toLocaleDateString('en-CA')}_${now.toTimeString().split(' ')[0]}`
+    return `invoice_${now.toLocaleDateString('en-CA')}_${
+      now.toTimeString().split(' ')[0]
+    }`
   }
 
   const generate = async () => {
-    if (values.invoice_date && values.seller_name && values.tax_amount && values.total && values.vat_number) {
+    if (
+      values.invoice_date &&
+      values.seller_name &&
+      values.tax_amount &&
+      values.total &&
+      values.vat_number
+    ) {
       const f = new Fatora({
         seller_name: values.seller_name,
         vat_number: values.vat_number,
         invoice_date: values.invoice_date,
         total: Number(values.total),
-        tax_amount: Number(values.tax_amount)
+        tax_amount: Number(values.tax_amount),
       })
 
       invoice_name = get_invoice_name()
@@ -47,66 +65,109 @@
       isToastOpened = true
       toastMessage = 'Please fill all the fields'
     }
+  }
 
+  const share_qrcode = async () => {
+    await shareFile({
+      url: qr_url,
+      filename: `${get_invoice_name()}.png`,
+      title: get_invoice_name()
+    })
+    .catch((r) => {
+      console.log('inside,', r)
+    })
   }
 
   onMount(() => {
-    const { seller_name, vat_number} = localStorage
+    const { seller_name, vat_number } = localStorage
     if (seller_name) values.seller_name = seller_name
     if (vat_number) values.vat_number = vat_number
     // yyyy-MM-ddThh:mm
     const now = new Date()
-    values.invoice_date = now.toLocaleDateString('en-CA')+'T'+now.toTimeString().split(' ')[0]
+    values.invoice_date =
+      now.toLocaleDateString('en-CA') + 'T' + now.toTimeString().split(' ')[0]
   })
-
 </script>
 
 <List>
-  <ListInput value={values.seller_name} onInput={(e) => onValueChange('seller_name', e.target.value)} label="Seller Name" outline floatingLabel={values.seller_name} />
-  <ListInput value={values.vat_number} onInput={(e) => onValueChange('vat_number', e.target.value)} label="Tax Number" outline floatingLabel={values.vat_number} />
-  <ListInput value={values.invoice_date} onInput={(e) => onValueChange('invoice_date', e.target.value)} type="datetime-local" defaultValue="now"
-    label="Invoice Date" outline />
-  <ListInput onInput={(e) => onValueChange('total', e.target.value)} type="number" label="Total" outline floatingLabel />
-  <ListInput onInput={(e) => onValueChange('tax_amount', e.target.value)} type="number" label="Tax Amount" outline floatingLabel />
+  <ListInput
+    value={values.seller_name}
+    onInput={(e) => onValueChange('seller_name', e.target.value)}
+    label="Seller Name"
+    outline
+    floatingLabel={values.seller_name}
+  />
+  <ListInput
+    value={values.vat_number}
+    onInput={(e) => onValueChange('vat_number', e.target.value)}
+    label="Tax Number"
+    outline
+    floatingLabel={values.vat_number}
+  />
+  <ListInput
+    value={values.invoice_date}
+    onInput={(e) => onValueChange('invoice_date', e.target.value)}
+    type="datetime-local"
+    defaultValue="now"
+    label="Invoice Date"
+    outline
+  />
+  <ListInput
+    onInput={(e) => onValueChange('total', e.target.value)}
+    type="number"
+    label="Total"
+    outline
+    floatingLabel
+  />
+  <ListInput
+    onInput={(e) => onValueChange('tax_amount', e.target.value)}
+    type="number"
+    label="Tax Amount"
+    outline
+    floatingLabel
+  />
   <Block>
     <Button onClick={generate}>Generate</Button>
   </Block>
 
   <Toast position="center" opened={isToastOpened}>
-    <Button
-    slot="button"
-    clear
-    inline
-    onClick={() => (isToastOpened = false)}
-    >
+    <Button slot="button" clear inline onClick={() => (isToastOpened = false)}>
       Close
     </Button>
-  <div class="shrink">{toastMessage}</div>
+    <div class="shrink">{toastMessage}</div>
   </Toast>
 
-  <Sheet 
-    opened={isOpen} 
-    onBackdropClick={() => isOpen = false}
+  <Sheet
+    opened={isOpen}
+    onBackdropClick={() => (isOpen = false)}
     class="pb-safe w-full"
     style="height: min(500px, 50vh)"
   >
-  <Toolbar top>
-    <div class="left">Your invoice</div>
-    <div class="right">
-      <Link toolbar onClick={() => (isOpen = false)}>Done</Link>
-    </div>
-  </Toolbar>
-
-      <div class="flex flex-col justify-center items-center gap-4 mt-4">
-          <img src={qr_url} alt="qrcode" />
-          <!-- TODO -->
-          <div class="footer w-full max-w-md flex gap-4">
-            <Link href={qr_url} download={invoice_name}>Download</Link>
-            <Button clear>share</Button>
-          </div>
+    <Toolbar top>
+      <div class="left">Your invoice</div>
+      <div class="right">
+        <Link
+          class="py-1 px-4 rounded-lg h-10 w-full"
+          onClick={() => (isOpen = false)}>Done</Link
+        >
       </div>
-  </Sheet>
+    </Toolbar>
 
+    <div class="flex flex-col justify-center items-center gap-4 mt-4">
+      <img src={qr_url} alt="qrcode" />
+      <!-- TODO -->
+      <div class="footer w-full max-w-md flex gap-4">
+        <Link class="h-10 overflow-hidden w-full rounded-lg" href={qr_url} download={invoice_name}>
+          <span class="material-symbols-outlined">download</span>
+          <span>Download</span>
+        </Link>
+        <Link onClick={share_qrcode} class="h-10 overflow-hidden w-full rounded-lg">
+          <span class="material-symbols-outlined">ios_share</span>
+          <span>Share</span>
+        </Link>
+      </div>
+    </div>
+  </Sheet>
 </List>
 
 <style>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,3 +18,34 @@ export function toTlv(tags: Tag[]) {
 export function toBase64(s: string) {
   return Buffer.from(s).toString('base64')
 }
+
+export interface FileShareOptions {
+  url: string,
+  filename: string,
+  title: string,
+  text?: string
+}
+
+export async function shareFile(opts: FileShareOptions) {
+  const blob = await fetch(opts.url).then(r => r.blob())
+
+  const data = {
+    files: [
+      new File([blob], opts.filename, {
+        type: blob.type,
+      })
+    ],
+    title: opts.title,
+    text: opts.text
+  }
+
+  try {
+    if (!navigator.canShare(data)) {
+      throw new Error('cant share')
+    }
+
+    await navigator.share(data)
+  } catch(err) {
+    console.error('Share Catch', err)
+  }
+}
