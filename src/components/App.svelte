95c32571638c225ddaae61f85bd720cbde741dac
omar2205 <omar2205a@yahoo.com>
2022-09-27 22:08:34 +0000

    update app and nav

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-  import { App, Block, Button, Navbar, Page } from 'konsta/svelte'
+  import { App, Block, Navbar, Page } from 'konsta/svelte'
   import Fatoora from './Forms/Fatoora.svelte'
 </script>
 
-<App theme='material' safeareas>
+<App theme="material" safeareas>
   <Page>
-    <Navbar medium title="eInvoice qrCode" />
+    <Navbar centerTitle title="eInvoice qrCode" />
     <slot />
-  <div class="relative max-w-md mx-auto">
-    <Block>
-      <p>Enter the following information</p>
-    </Block>
-    <Fatoora />
-  </div>
+    <div class="relative max-w-md mx-auto">
+      <Block>
+        <p>Enter the following information</p>
+      </Block>
+      <Fatoora />
+    </div>
+    <p class="text-sm text-center dark:text-gray-600 text-gray-400">Your data is private. Nothing is being stored or logged.</p>
   </Page>
 </App>
