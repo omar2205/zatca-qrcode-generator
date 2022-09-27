71002a6be0c529f685ecd34a2c74a52bfc4e36fc
omar2205 <omar2205a@yahoo.com>
2022-09-27 09:25:54 +0000

    add netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import tailwind from '@astrojs/tailwind'
 
 import svelte from '@astrojs/svelte'
 
+import netlify from '@astrojs/netlify/edge-functions'
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
+  output: 'server',
+  adapter: netlify()
 })
